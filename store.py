5c30ece0004ca1937daa12577dd4c1db89cd2b63
Deborah Sutton <sutton.deborah@gmail.com>
2019-07-22 00:23:50 +0300

    final version

--- store.py
+++ store.py
@@ -1,6 +1,6 @@
 import json
 
-from bottle import run, template, static_file, get, post, delete, request
+from bottle import run, template, static_file, get, post, request, delete
 from pymysql import connect, cursors
 
 connection = connect(host='127.0.0.1',
@@ -43,76 +43,68 @@ def images(filename):
 
 @post('/category')
 def add_category():
-    category_name = request.forms.get("name")
-    with connection.cursor() as cursor:
-        sql = "select count(1) from category where cat_name='{}'".format(category_name)
-        cursor.execute(sql)
-        result = cursor.fetchall()
+    try:
+        category_name = request.forms.get("name")
+        with connection.cursor() as cursor:
+            sql = "select * from category where cat_name='{}'".format(category_name)
+            count = cursor.execute(sql)
+            if count >= 1:
+                return ({"STATUS": "ERROR", "MSG": "The category was not created due to an error - category already exists",
+                         "code": "200"})
 
-        if result == [{'count(1)': 1}]:
-            return ({"STATUS": "ERROR - The category was not created due to an error", "MSG": "category already exists",
-                     "code": "200"})
+            elif not category_name.strip():
 
-        elif category_name == " ":
-
-            return (
-            {"STATUS": "ERROR - The category was not created due to an error", "MSG": "Name parameter is missing",
-             "code": "400"})
-
-
-        elif result == [{'count(1)': 0}]:
-            adding_category = 'INSERT INTO category (cat_name)  values ("{}")'.format(category_name)
-            cursor.execute(adding_category)
-            connection.commit()
-            return json.dumps(
-                {'STATUS': 'SUCCESS', "MSG": "The category was created successfully", {cat_id: cursor.lastrowid},
-                 "code":"201"})
+                return (
+                    {"STATUS": "ERROR", "MSG": "The category was not created due to an error - Name parameter is missing",
+                     "code": "400"})
 
             else:
-            return ({
-                "STATUS": "ERROR - The category was not created due to an error", "MSG": "Internal error",
-                "code": "500"})
-
+                adding_category = 'INSERT INTO category (cat_name)  values ("{}")'.format(category_name)
+                cursor.execute(adding_category)
+                connection.commit()
+                return json.dumps(
+                    {'STATUS': 'SUCCESS', 'MSG': 'The category was created successfully', 'CAT_ID': cursor.lastrowid,
+                    'code':'201'})
+    except Exception as e:
+        return ({
+            "STATUS": "ERROR", "MSG": "Internal error - The category was not created due to an error - " + str(e),
+            "code": "500"})
 
 @delete('/category/<id>')
 def delete_category(id):
-    with connection.cursor() as cursor:
-        sql = "select count(1) from category where cat_name='{}'".format(category_name)
-        cursor.execute(sql)
-        result = cursor.fetchall()
+    try:
+        with connection.cursor() as cursor:
+            sql = "select * from category where cat_id='{}'".format(id)
+            count = cursor.execute(sql)
+        if count == 0:
+            return ({"STATUS": "ERROR", "MSG": "The category was not deleted due to an error - Category not found",
+                  "code": "404"})
 
-        if result == [{'count(1)': 0}]:
-            return ({"STATUS": "ERROR - The category was not deleted due to an error", "MSG": "Category not found",
-                     "code": "404"})
-
-        elif result == [{'count(1)': 1}]:
+        elif count == 1:
             sqlDelete_cat = "DELETE FROM CATEGORY where cat_id={}".format(id)
             cursor.execute(sqlDelete_cat)
             connection.commit()
             return json.dumps({"STATUS": "SUCCESS - The category was deleted successfully", "code": "201"})
 
-        else:
-            return json.dumps(
-                {"STATUS": "ERROR - The category was not deleted due to an error", "MSG": "internal error",
-                 "code": "500"})
+    except Exception as e:
+        return json.dumps(
+            {"STATUS": "ERROR", "MSG": "Internal error - The category was not deleted due to an error - " + str(e),
+             "code": "500"})
 
 
 @get('/categories')
 def list_categories():
     try:
         categories = []
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM categories"
-        cursor.execute(sql)
-        result = cursor.fetchall()
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM category"
+            cursor.execute(sql)
+            result = cursor.fetchall()
         for element in result:
             cat_to_add = {"id": element['cat_id'], "name": element["cat_name"]}
             categories.append(cat_to_add)
-            return json.dumps({"STATUS": "SUCCESS - Categories fetched", "code": "200"})
-        except:
-        return json.dumps({"STATUS": "ERROR -internal error", "MSG": "Internal error", code": "500"})
+        return json.dumps({"STATUS": "SUCCESS", "MSG":"Categories fetched", "code": "200", "CATEGORIES":categories})
+    except Exception as e:
+        return json.dumps({"STATUS": "ERROR", "MSG": "Internal error - " + str(e), "code": "500"})
 
-
-
-
-                           run(host='localhost', port=7000)
+run(host='localhost', port=7000)
